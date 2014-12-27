5ffe28f7e0fe6ae60b04b7c6541178d31676511a
Rashed Abdel-Tawab <rashed@linux.com>
2014-12-27 02:21:09 +0200

    ls980: Add device specific gps.conf
    Change-Id: I1a03380ca54f30e0664056b264bbfb8e339bf1b9

--- device.mk
+++ device.mk
@@ -31,3 +31,7 @@ PRODUCT_PACKAGES += \
 # NFC config
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf
+
+# GPS
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf
